f0be447a2809f771fbd3604df02a1c8d0e4c019d
Elusive <elusivecodes@gmail.com>
2022-04-04 18:22:42 +1000

    Removed FyreError dependency

--- src/Exceptions/EncryptionException.php
+++ src/Exceptions/EncryptionException.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 namespace Fyre\Encryption\Exceptions;
 
 use
-    Fyre\Error\Exceptions\Exception;
+    RuntimeException;
 
 /**
  * EncryptionException
  */
-class EncryptionException extends Exception
+class EncryptionException extends RuntimeException
 {
 
     public static function forConfigExists(string $key)
